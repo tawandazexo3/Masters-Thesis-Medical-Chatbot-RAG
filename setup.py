cc6483111bea75a7997a7e137874e86f9ff3fed7
git config user.name tawandazexo3 <tawandazexo@gmail.com>
2025-10-23 16:54:22 +0200

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Tawanda Bozho',
+    author_email= 'tawandazexo@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
